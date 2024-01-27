2cc1ce9127694faea10a8048c524d5eff9179f03
Dat Nguyen <datnguyen.it09@gmail.com>
2024-01-27 10:20:55 +0700

    feat: add graphql helper

---
+++ dbterd/helpers/graphql.py
@@ -0,0 +1,39 @@
+import requests
+
+from dbterd.helpers.log import logger
+
+
+class GraphQLHelper:
+    def __init__(self, **kwargs) -> None:
+        self.host_url = kwargs.get("dbt_cloud_host_url")
+        self.service_token = kwargs.get("dbt_cloud_service_token")
+
+    @property
+    def request_headers(self) -> dict:
+        """API Header"""
+        return {
+            "authorization": f"Bearer {self.service_token}",
+            "content-type": "application/json",
+        }
+
+    @property
+    def api_endpoint(self) -> dict:
+        """Base GraphQL API endpoint"""
+        return f"https://{self.host_url}/graphql/"
+
+    def query(self, query: dict, **variables):
+        try:
+            r = requests.post(
+                self.api_endpoint,
+                headers=self.request_headers,
+                json={"query": query, "variables": variables},
+            )
+
+            if r.status_code != 200:
+                logger.error(f"Failed to query [error: {vars(r)}]")
+                return None
+        except Exception as e:
+            logger.error(f"Error occurred while querying [error: {str(e)}]")
+            return None
+
+        return r.json().get("data")
