8bb074729db99b75d8a4b9fc9f82626c792843f1
Dat Nguyen <datnguyen.it09@gmail.com>
2024-02-07 15:46:21 +0700

    chore: fullfil unit test up to 96%

--- tests/unit/adapters/test_base.py
+++ tests/unit/adapters/test_base.py
@@ -10,6 +10,50 @@ from dbterd.adapters.dbt_core.dbt_invocation import DbtInvocation
 
 
 class TestBase:
+    @mock.patch("dbterd.adapters.base.Executor.evaluate_kwargs")
+    @mock.patch("dbterd.adapters.base.Executor._Executor__run_metadata_by_strategy")
+    def test_run_metadata(self, mock_run_metadata_by_strategy, mock_evaluate_kwargs):
+        Executor(ctx=click.Context(command=click.BaseCommand("dummy"))).run_metadata()
+        mock_evaluate_kwargs.assert_called_once()
+        mock_run_metadata_by_strategy.assert_called_once()
+
+    @mock.patch("dbterd.adapters.base.DbtCloudMetadata.query_erd_data")
+    @mock.patch("dbterd.adapters.base.Executor._Executor__save_result")
+    def test___run_metadata_by_strategy(self, mock_query_erd_data, mock_save_result):
+        Executor(
+            ctx=click.Context(command=click.BaseCommand("dummy"))
+        )._Executor__run_metadata_by_strategy(target="dbml", algo="test_relationship")
+        mock_query_erd_data.assert_called_once()
+        mock_save_result.assert_called_once()
+
+    @mock.patch("builtins.open")
+    def test___save_result(self, mock_open):
+        Executor(
+            ctx=click.Context(command=click.BaseCommand("dummy"))
+        )._Executor__save_result(path="irrelevant", data=("file_name", {}))
+        mock_open.assert_called_once_with("irrelevant/file_name", "w")
+
+    @mock.patch("dbterd.adapters.base.DbtCloudArtifact.get")
+    @mock.patch("dbterd.adapters.base.Executor._Executor__read_manifest")
+    @mock.patch("dbterd.adapters.base.Executor._Executor__read_catalog")
+    @mock.patch("dbterd.adapters.base.Executor._Executor__save_result")
+    def test___run_by_strategy_w_dbt_cloud(
+        self,
+        mock_cloud_artifact_get,
+        mock_read_manifest,
+        mock_read_catalog,
+        mock_save_result,
+    ):
+        Executor(
+            ctx=click.Context(command=click.BaseCommand("dummy"))
+        )._Executor__run_by_strategy(
+            target="dbml", algo="test_relationship", dbt_cloud=True
+        )
+        mock_cloud_artifact_get.assert_called_once()
+        mock_read_manifest.assert_called_once()
+        mock_read_catalog.assert_called_once()
+        mock_save_result.assert_called_once()
+
     def test_worker(self):
         worker = Executor(ctx=click.Context(command=click.BaseCommand("dummy")))
         assert worker.filename_manifest == "manifest.json"
@@ -59,9 +103,10 @@ class TestBase:
             worker._Executor__get_selection()
 
     @pytest.mark.parametrize(
-        "kwargs, expected",
+        "command, kwargs, expected",
         [
             (
+                "run",
                 dict(
                     select=[],
                     exclude=[],
@@ -74,6 +119,7 @@ class TestBase:
                 ),
             ),
             (
+                "run",
                 dict(select=[], exclude=[], dbt=True),
                 dict(
                     dbt=True,
@@ -84,6 +130,7 @@ class TestBase:
                 ),
             ),
             (
+                "run",
                 dict(select=[], exclude=[], dbt=True, dbt_auto_artifacts=True),
                 dict(
                     dbt=True,
@@ -95,6 +142,7 @@ class TestBase:
                 ),
             ),
             (
+                "run",
                 dict(select=[], exclude=[], dbt_cloud=True),
                 dict(
                     dbt_cloud=True,
@@ -114,16 +162,20 @@ class TestBase:
         mock_get_artifacts_for_erd,
         mock_get_selection,
         mock_get_dir,
+        command,
         kwargs,
         expected,
     ):
-        worker = Executor(ctx=click.Context(command=click.BaseCommand("run")))
+        worker = Executor(ctx=click.Context(command=click.BaseCommand(command)))
         mock_get_dir.return_value = ("/path/ad", "/path/dpd")
         mock_get_selection.return_value = ["yolo"]
         assert expected == worker.evaluate_kwargs(**kwargs)
         mock_get_dir.assert_called_once()
-        if kwargs.get("dbt_auto_artifacts"):
-            mock_get_artifacts_for_erd.assert_called_once()
+        if command == "run":
+            if kwargs.get("dbt") and kwargs.get("dbt_auto_artifacts"):
+                mock_get_artifacts_for_erd.assert_called_once()
+        else:
+            assert mock_get_artifacts_for_erd.called_count == 0
 
     @pytest.mark.parametrize(
         "kwargs, mock_isfile_se, expected",
