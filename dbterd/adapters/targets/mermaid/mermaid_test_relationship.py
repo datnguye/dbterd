c8339f77f80949a1cc530b93cdef39581eaaf56a
Yasuhisa Yoshida <syou6162@gmail.com>
2024-02-08 12:05:13 +0900

    Refactoring to use key_from and key_to

--- dbterd/adapters/targets/mermaid/mermaid_test_relationship.py
+++ dbterd/adapters/targets/mermaid/mermaid_test_relationship.py
@@ -45,9 +45,9 @@ def parse(manifest, catalog, **kwargs):
     for rel in relationships:
         key_from = f'"{rel.table_map[1]}"'
         key_to = f'"{rel.table_map[0]}"'
-        reference_text = rel.column_map[0].replace(" ", "-")
-        if rel.column_map[0] != rel.column_map[1]:
-            reference_text += f"--{ rel.column_map[1].replace(' ','-')}"
+        reference_text = key_to.replace(" ", "-")
+        if key_to != key_from:
+            reference_text += f"--{ key_from.replace(' ','-')}"
         mermaid += f"  {key_from.upper()} {get_rel_symbol(rel.type)} {key_to.upper()}: {reference_text}\n"
 
     return mermaid
