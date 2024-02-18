13d064b470659b53731ff5fe3196a939f6f59720
Dat Nguyen <datnguyen.it09@gmail.com>
2024-02-18 15:29:57 +0700

    feat: skeleton- dbterd api support officially

---
+++ dbterd/api/base.py
@@ -0,0 +1,12 @@
+class DbtErd:
+    # from dbterd.api.base import DbtErd
+    # DbtErd().get_erd()        # return a whole erd entities
+    # DbtErd().get_model_erd()  # return this model's entity and 1 level relationship entities if any
+    def __init__(self) -> None:
+        pass
+
+    def get_erd(self):
+        pass
+
+    def get_model_erd(self):
+        pass
