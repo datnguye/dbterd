b9c9b95b265ca80f4122b90943d5c217da60e941
Dat Nguyen <datnguyen.it09@gmail.com>
2024-02-18 16:43:52 +0700

    fix: docs build failed [skip ci]

--- docs/hooks.py
+++ docs/hooks.py
@@ -6,6 +6,6 @@ import subprocess
 def on_post_build(config, **kwargs):
     site_dir = config["site_dir"]
     subprocess.run(
-        ["pdoc", "-t", "docs/assets/css", "dbterd/api/base.py", "-o", "target/pdoc"]
+        ["pdoc", "-t", "./docs/assets/css", "./dbterd/api/base.py", "-o", "./target/pdoc"]
     )
     shutil.copytree("target/pdoc", os.path.join(site_dir, "api-docs"))
