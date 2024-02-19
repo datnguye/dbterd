bfa6f1697307c68113b4fdd079850132719b9008
Dat Nguyen <datnguyen.it09@gmail.com>
2024-02-19 15:39:03 +0700

    feat: add load_algo

--- dbterd/adapters/adapter.py
+++ dbterd/adapters/adapter.py
@@ -19,3 +19,22 @@ def load_executor(name: str):
         if exc.name == "dbterd.adapters.targets." + name:
             raise Exception(f"Could not find adapter target type {name}!")
         raise  # pragma: no cover
+
+def load_algo(name: str):
+    """Import the algo extension dynamically
+
+    Args:
+        name (str): Algo name e.g. test_relationship
+
+    Raises:
+        Exception: Algo not found
+
+    Returns:
+        ModuleType: Imported module
+    """
+    try:
+        return import_module(name=f".{name}", package="dbterd.adapters.algos")
+    except ModuleNotFoundError as exc:
+        if exc.name == "dbterd.adapters.algos." + name:
+            raise Exception(f"Could not find adapter algo {name}!")
+        raise  # pragma: no cover
