f44023b63f6af0305369264043064c88d87f82ce
Dat Nguyen <datnguyen.it09@gmail.com>
2025-04-24 06:40:25 +0700

    chore: remove segement tracking from the docs site

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -146,21 +146,4 @@ async function main() {
     setupTermynal();
 }
 
-
-!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="2NV1THqXEM16c117579IOuZUr2NeQA5v";;analytics.SNIPPET_VERSION="4.15.3";
-  analytics.load("2NV1THqXEM16c117579IOuZUr2NeQA5v");
-  analytics.page();
-}}();
-
-document.querySelectorAll('a.md-nav__link').forEach(link => {
-    if (link.href.includes('#')) {
-      link.addEventListener('click', (e) => {
-        // Retrieve href and store in targetUrl variable
-        let targetUrl = e.target.href;
-        analytics.track('Link Clicked', {'link': targetUrl});
-      });
-    }
-
-});
-
 main()
